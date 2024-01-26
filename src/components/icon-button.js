3235ca5ff37cd9bc45a7fcaf59560f0d3a74f38e
temelm <mustafatemel90@gmail.om>
2024-01-26 15:13:59 +0000

    Add icon button component

---
+++ src/components/icon-button.js
@@ -0,0 +1,38 @@
+import { isNonEmptyString } from '../utils.js';
+
+/**
+ * @todo: DOCUMENT.
+ * @param {string} icon
+ * @param {object} [options]
+ */
+export function createIconButton (icon, options = {}) {
+  if (!isNonEmptyString(icon)) {
+    return false;
+  }
+
+  if (isNonEmptyString(options.id) && document.getElementById(options.id)) {
+    return false;
+  }
+
+  const button = document.createElement('button');
+
+  button.classList.add('icon-button');
+
+  if (options.disabled) {
+    button.setAttribute('disabled', true);
+  }
+
+  if (isNonEmptyString(options.id)) {
+    button.setAttribute('id', options.id);
+  }
+
+  if (isNonEmptyString(options.title)) {
+    button.setAttribute('title', options.title);
+  }
+
+  button.innerHTML = `
+    <i class="${icon}"></i>
+  `;
+
+  return button;
+}
