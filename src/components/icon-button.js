70b4eeb3515a6f17439f6c4d89e2985819f54a61
temelm <mustafatemel90@gmail.om>
2024-01-30 20:13:25 +0000

    Refacotor icon button component, add checkbox component

--- src/components/icon-button.js
+++ src/components/icon-button.js
@@ -4,39 +4,19 @@ import { isNonEmptyString } from '../utils.js';
  * @todo: DOCUMENT.
  * @param {string} icon
  * @param {object} [options]
+ * @returns {HTMLElement|boolean}
  */
 export function createIconButton (icon, options = {}) {
-  if (!isNonEmptyString(icon)) {
-    return false;
-  }
+  if (!isNonEmptyString(icon)) return false;
+  if (isNonEmptyString(options.id) && document.getElementById(options.id)) return false;
 
-  if (isNonEmptyString(options.id) && document.getElementById(options.id)) {
-    return false;
-  }
+  const domIconButton = document.createElement('button');
+  domIconButton.innerHTML = `<i class="${icon}"></i>`;
+  domIconButton.classList.add('icon-button');
+  if (isNonEmptyString(options.id)) domIconButton.id = options.id;
+  if (isNonEmptyString(options.title)) domIconButton.title = options.title;
+  if (options.disabled) domIconButton.disabled = true;
+  if (typeof options.onClick === 'function') domIconButton.addEventListener('click', options.onClick);
 
-  const button = document.createElement('button');
-
-  button.classList.add('icon-button');
-
-  if (options.disabled) {
-    button.setAttribute('disabled', true);
-  }
-
-  if (isNonEmptyString(options.id)) {
-    button.setAttribute('id', options.id);
-  }
-
-  if (isNonEmptyString(options.title)) {
-    button.setAttribute('title', options.title);
-  }
-
-  if (typeof options.onClick === 'function') {
-    button.addEventListener('click', options.onClick);
-  }
-
-  button.innerHTML = `
-    <i class="${icon}"></i>
-  `;
-
-  return button;
+  return domIconButton;
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import './index.css';
 import { createIconButton } from './components/icon-button.js';
+import { createCheckbox } from './components/checkbox.js';
 
 const domMain = document.querySelector('main');
-domMain.appendChild(createIconButton('fas fa-cat', {
-  id: 'cat-button',
-  title: 'Cat',
+
+domMain.appendChild(document.createElement('br'));
+domMain.appendChild(document.createElement('br'));
+
+domMain.appendChild(createIconButton('fas fa-cog', {
+  id: 'settings-button',
+  title: 'Settings',
+  disabled: false,
   onClick: () => {
-    alert('Meow');
+    console.log('Settings button clicked.');
+  }
+}));
+
+domMain.appendChild(document.createElement('br'));
+domMain.appendChild(document.createElement('br'));
+
+domMain.appendChild(createCheckbox('Preserve log', {
+  id: 'preserve-log-checkbox',
+  title: 'Preserve log',
+  checked: false,
+  disabled: true,
+  onClick: () => {
+    console.log('Preserve log checkbox clicked.');
   }
 }));
