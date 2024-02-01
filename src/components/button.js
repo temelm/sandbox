8d98d7ab90657f8139532eb88aa4224739d9e455
temelm <mustafatemel90@gmail.om>
2024-02-01 13:40:49 +0000

    Refactor button component

--- src/components/button.js
+++ src/components/button.js
@@ -2,21 +2,25 @@ import { isNonEmptyString } from '../utils.js';
 
 /**
  * @todo: DOCUMENT.
- * @param {string} label
- * @param {object} [options]
+ * @param {object} options
  * @returns {HTMLButtonElement|boolean}
  */
-export function createButton (label, options = {}) {
-  if (!isNonEmptyString(label)) return false;
-  if (isNonEmptyString(options.id) && document.getElementById(options.id)) return false;
+export function createButton (options = {}) {
+  const { label, icon, id, title, disabled, onClick } = options;
+
+  if (!isNonEmptyString(label) && !isNonEmptyString(icon)) return false;
 
   const domButton = document.createElement('button');
-  domButton.innerHTML = `<span>${label}</span>`;
+  domButton.innerHTML = `
+    ${icon ? `<span><i class="${icon}"></i></span>` : ''}
+    ${label ? `<span>${label}</span>` : ''}
+  `;
   domButton.classList.add('button');
-  if (isNonEmptyString(options.id)) domButton.id = options.id;
-  if (isNonEmptyString(options.title)) domButton.title = options.title;
-  if (options.disabled) domButton.disabled = true;
-  if (typeof options.onClick === 'function') domButton.addEventListener('click', options.onClick);
+  if (!label) domButton.classList.add('icon-button');
+  if (isNonEmptyString(id)) domButton.id = id;
+  if (isNonEmptyString(title)) domButton.title = title;
+  if (disabled) domButton.disabled = true;
+  if (typeof onClick === 'function') domButton.addEventListener('click', onClick);
 
   return domButton;
 }
