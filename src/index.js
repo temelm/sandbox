913453102a26ec9653afb5f2615026d6edbb717f
temelm <mustafatemel90@gmail.com>
2024-02-02 15:18:25 +0000

    Update favicon, add base CSS and JS

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+
+console.log('Hello, world!');
+
+const domH1 = document.querySelector('h1');
+if (domH1) {
+  domH1.style.marginTop = '1rem';
+  domH1.style.textAlign = 'center';
+}
